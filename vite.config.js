2cc56e31b44537a06cc03a776d40fb266acfd065
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2024-05-14 16:42:39 -0300

    Changed route

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://fabrizioblanco.github.io/step-1"
+  base: "/step-1/"
   // base: "https://FabrizioBlanco.github.io/multi.step.form.main"
 })
